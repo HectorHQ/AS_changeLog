23131edead6101577f0262f38ca3e926cc477e48
HectorHQ <109182400+HectorHQ@users.noreply.github.com>
2024-06-04 17:55:17 -0600

    Update logs_qbo_integration.py

--- logs_qbo_integration.py
+++ logs_qbo_integration.py
@@ -204,7 +204,8 @@ def pending_deductions(df_applications):
     pending_deductions_data = pd.DataFrame(list_pending_deductions)
 
     #pending_deductions_data_grp = pending_deductions_data.groupby('orderNumber').agg({'amount':'sum','eligbleAt':'first','qbCustomerPaidById':'first','qbClassInvoiceBrandOrg':'first','qbCustomerPaidToId':'first','createdat':'first','changeTag':'first'}).reset_index()
-    deductions_df = pending_deductions_data_grp.copy()
+    #deductions_df = pending_deductions_data_grp.copy()
+    deductions_df = pending_deductions_data.copy()
 
     return deductions_df,pending_deductions_data
 
